39f773390f0462478c571e0de6cddd6ab0b92586
Fixie Radish <fixierad@student.21-school.ru>
2023-01-23 21:56:02 +0300

    add struct

--- s21_3DViewer.h
+++ s21_3DViewer.h
@@ -1,35 +1,46 @@
 #ifndef SRC_S21_3DVIEVER_H_
 #define SRC_S21_3DVIEVER_H_
 
+#include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
 
 // typedef struct points_struct{
-  
+
 // } points;
 
 typedef struct vertex_struct {
-  double x,y,z;
-}vertex;
+  double x, y, z;
+} vertex;
+// vertex ->  *ptr= calloc ( amount_ver, sizeof(vertex_struct);
+// ptr[index] = calloc(3, sizeof(double));
+// ptr[index][x] = 1 , 2 , 3
 
-typedef struct poiligons_struct{
+//  amount of vertex ;
+// ptrofpol[index] = calloc (amoun, sizeof(int));
+// ptrofpol[index][ 0 1 2 3 ] = int;
+
+typedef struct poiligons_struct {
   int *p;
   int amount_pol;
 } poligons;
 
 typedef struct exit_struct {
-  int minmax_coor[2];
-  int amount_pol;
-  int amount_ver;
+  int minmaxX[2];
+  int minmaxY[2];
+  int minmaxZ[2];
+  int amount_struct_pol;
+  int amount_struct_ver;
   vertex *v;
   poligons *p;
-}exit_st;
+} exit_st;
 
 exit_st s21_parse(char *filename, int *status, int *amount_ver,
                   int *amount_pol);
+int s21_amount_value(char *filename, int *status, exit_st *st);
+int s21_init_struct(exit_st);
 
-#endif //SRC_S21_3DVIEVER_H_
+#endif  // SRC_S21_3DVIEVER_H_
--- s21_parse.c
+++ s21_parse.c
@@ -4,14 +4,15 @@ int main() {
   int status = 0;
   int amount_ver = 0;
   int amount_pol = 0;
-  s21_parse("test.obj", &status, &amount_ver, &amount_ver);
-    //printf("%d %d\n", amount_ver, amount_pol);
+  exit_st st= {0};
+  s21_amount_value("FinalBaseMesh.obj", &status,&st);
+  // printf("%d %d\n", amount_ver, amount_pol);
   return 0;
 }
 
-exit_st s21_parse(char *filename, int *status, int *amount_ver,
-                  int *amount_pol) {
-  exit_st st = {0};
+exit_st s21_parse() { return st; }
+
+int s21_amount_value(char *filename, int *status, exit_st *st) {
   ssize_t read = 0;
   char *line = NULL;
   size_t size_line = 0;
@@ -19,21 +20,31 @@ exit_st s21_parse(char *filename, int *status, int *amount_ver,
   char sym = ' ';
   if (file != NULL) {
     while ((read = getline(&line, &size_line, file)) != -1) {
-      if (strcmp("v",line)) {
-        *amount_ver+=1;
-        //   printf("debug");
+      if (line[0] == 'v' && line[1] == ' ') {
+        st->amount_struct_ver += 1;
       }
-    
-      if (strcmp("f",line)) {
-        *amount_pol+=1;
+
+      if (line[0] == 'f' && line[1] == ' ') {
+        st->amount_struct_pol += 1;
       }
     }
-    
+
     free(line);
   }
-  printf("%d ", *amount_pol);
-  printf("%d\n", *amount_ver);
-    
+  printf("%d ", st->amount_struct_ver);
+  printf("%d\n", st->amount_struct_pol);
+
   fclose(file);
-  return st;
 }
+
+int s21_init_struct(exit_st *st) { 
+  int status =0;
+   if ((st->p =(poligons*) calloc(st->amount_struct_pol,sizeof(poligons))) == NULL){
+    status =1;
+   } 
+
+  if ((st->v =(vertex*) calloc(st->amount_struct_ver,sizeof(vertex))) == NULL){
+    status =1;
+  }
+  return status;
+ }
