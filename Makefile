91ff1d2ffe6e0ba0b1b22aca681b0d05ca3a8af8
Vladimir_laptop <vladimir.frolov.99@mail.ru>
2023-08-21 19:57:07 +0300

    files from gitlab

--- Makefile
+++ Makefile
@@ -3,14 +3,12 @@ CFLAGS=-Wall -Werror -Wextra
 GCOVFLAGS = -fprofile-arcs -ftest-coverage
 SRCS=$(wildcard s21_*.c)
 OBJS=$(SRCS:.c=.o)
-TESTSFLAGS_MAC= -lcheck -lpthread -lsubunit -lrt -lm
-LIB_NAME=smart_calc
+TESTSFLAGS_MAC= -lcheck 
+# -lpthread -lsubunit -lrt -lm
+LIB_NAME=3d_viewer
 
-all: run
+all: install
 
-run:
-	gcc *.c -o for_test
-	./for_test
 to_style:
 	clang-format -style=google -i *.c *.cpp *.h
 
@@ -28,22 +26,22 @@ $(LIB_NAME).a: $(OBJS)
 
 install: 
 	mkdir Qmake
-	cd Qmake/ && qmake ../Smart_Calc.pro && make 
+	cd Qmake/ && qmake ../Viewer.pro && make 
 	
 open:
-	open Qmake/Smart_Calc.app
+	cd Qmake && ./Viewer
 
 uninstall:
 	rm -rf Qmake
 
 dist:
-	mkdir SmartCalc
-	cp -r *.c *.h *.cpp *.ui *.pro Makefile tests/ SmartCalc
-	tar -cvzf dist.tar.gz SmartCalc
-	-rm -rf SmartCalc
+	mkdir Viewer
+	cp -r *.c *.h *.cpp *.ui *.pro Makefile tests/ Viewer
+	tar -cvzf dist.tar.gz Viewer
+	-rm -rf Viewer
 
 
-test: $(LIB_NAME).a 
+tests: $(LIB_NAME).a 
 	$(CC) $(CFLAGS) tests/test.c -L. $(LIB_NAME).a $(TESTSFLAGS_MAC) -o test
 	./test
 
@@ -51,7 +49,7 @@ test: $(LIB_NAME).a
 add_coverage_flag:
 	$(eval CFLAGS += --coverage)
 
-gcov_report: add_coverage_flag test
+gcov_report: add_coverage_flag tests
 	./test
 	lcov -t "test" -o Coverage_Report.info -c -d .
 	genhtml -o ./report Coverage_Report.info
@@ -59,6 +57,6 @@ gcov_report: add_coverage_flag test
 	open ./report/index-sort-f.html
 
 clean:
-	rm -rf *.o test *.a *.gcno *.gcda *.gcov *.html *.css *.out *.info report Qmake html dist.tar.gz SmartCalc for_test
+	rm -rf *.o test *.a *.gcno *.gcda *.gcov *.html *.css *.out *.info report Qmake html dist.tar.gz Viewer
 
 rebuild: clean all 
--- s21_parse.c
+++ s21_parse.c
@@ -1,71 +1,60 @@
+#include <locale.h>
+
 #include "s21_3DViewer.h"
+// int main() {
 
-int main() {
+//  exit_st *st = (exit_st *)calloc(1, sizeof(exit_st));
+// s21_parse(st, "test.obj");
+//  // printf("%d", st->amount_struct_ver);
+//  for (unsigned i = 0; i < st->amount_struct_ver; i++) {
+//    printf("v %lf ", st->vertex[i]);
+//    printf("%lf ", st->vertex[i + 1]);
+//    printf("%lf\n", st->vertex[i + 2]);
+//    i += 2;
+//  }
+//  // for (int i = 0; i < st->amount_struct_pol; i++) {
+//  //   printf(" %d", st->poligons[i]);
+//  // }
+//  // printf("%d", st->amount_struct_pol);
+//  // printf("%lf ", st->minmaxY[0]);
+//  // printf("%lf", st->minmaxY[1]);
+//  // double scale = normalize(st);
+//  // set_scale(st, scale);
+//  // printf("%lf ", st->vertex[0]);
+//  // printf("%lf ", st->vertex[1]);
+//  // printf("%lf", st->vertex[2]);
+//  s21_remove_struct(st);
+//  free(st);
+//  return 0;
+//}
+int s21_parse(exit_st *st, char *filename) {
+  setlocale(LC_ALL, "en-US");
+  setlocale(LC_NUMERIC, "C");
   int status = 0;
-  int amount_ver = 0;
-  int amount_pol = 0;
-  exit_st st = {0};
-  st = s21_parse("test.obj");
-  printf("%lf ", st.v[0].x);
-  printf("%lf ", st.v[0].y);
-  printf("%lf\n", st.v[0].z);
-  printf("%d\n", st.p[0].poligon[0]);
-  printf("%lf ", st.minmaxX[0]);
-  printf("%lf", st.minmaxX[1]);
-  return 0;
-}
-
-exit_st s21_parse(char *filename) {
-  exit_st *st = (exit_st *)calloc(1, sizeof(exit_st));
-  int status = 0;
-  status = s21_fill_amount(filename, st);
-  s21_init_struct(st);
-  //KOSTIIIIIL
-  st->minmaxX[1]=-9999;
-  st->minmaxY[1]=-9999;
-  st->minmaxZ[1]=-9999;
-  st->minmaxX[0]=9999;
-  st->minmaxY[0]=9999;
-  st->minmaxZ[0]=9999;
-  ssize_t read = 0;
-  char *line = NULL;
-  size_t size_line = 0;
-  FILE *file = fopen(filename, "r");
-  char *ptr = line;
-  int vertex_counter = 0;
-  int poligons_counter = 0;
-  while ((read = getline(&line, &size_line, file)) != -1) {
-    ptr = line;
-    if (*ptr == 'v') {
-      parse_vertex(&(st->v[vertex_counter]), ptr,st);
-      vertex_counter++;
-    } else if (*ptr == 'f') {
-      memory_for_pol(ptr, &(st->p[poligons_counter]));
-      ptr = line;
-      parse_poligons(ptr, &(st->p[poligons_counter]));
-      poligons_counter++;
-    }
-  }
-  return *st;
-}
-
-int s21_fill_amount(char *filename, exit_st *st) {
-  int status = 0;
-  ssize_t read = 0;
   char *line = NULL;
   size_t size_line = 0;
   FILE *file = fopen(filename, "r");
   if (file != NULL) {
-    while ((read = getline(&line, &size_line, file)) != -1) {
-      if (line[0] == 'v' && line[1] == ' ') {
-        st->amount_struct_ver += 1;
-      }
-      if (line[0] == 'f' && line[1] == ' ') {
-        st->amount_struct_pol += 1;
+    char *ptr = NULL;
+    int poligons_counter = 0;
+    int vertex_counter = 0;
+    int capacity_ver = 3;
+    int capacity_pol = 3;
+    status = s21_init_struct(st);
+    if (!status) {
+      while (getline(&line, &size_line, file) != -1 && !status) {
+        ptr = line;
+        if (*ptr == 'v') {
+          status = parse_vertex(&vertex_counter, ptr, st, &capacity_ver);
+        } else if (*ptr == 'f') {
+          status = parse_poligons(ptr, st, &poligons_counter, &capacity_pol);
+        }
       }
     }
     free(line);
     fclose(file);
+    st->amount_struct_pol = poligons_counter;
+    st->amount_struct_ver = vertex_counter;
   } else {
     status = 1;
   }
@@ -74,88 +63,140 @@ int s21_fill_amount(char *filename, exit_st *st) {
 
 int s21_init_struct(exit_st *st) {
   int status = 0;
-  if ((st->p = (poligons *)calloc(st->amount_struct_pol, sizeof(poligons))) ==
-      NULL) {
+  st->amount_struct_pol = 0;
+  st->amount_struct_ver = 0;
+  if ((st->poligons = (unsigned *)calloc(3, sizeof(unsigned))) == NULL) {
+    status = 1;
   }
-  if ((st->v = (vertex *)calloc(st->amount_struct_ver, sizeof(vertex))) ==
-      NULL) {
+  if ((st->vertex = (double *)calloc(3, sizeof(double))) == NULL) {
     status = 1;
   }
   return status;
 }
 
-void parse_vertex(vertex *v, char *ptr, exit_st *st) {
+int parse_vertex(int *vertex_counter, char *ptr, exit_st *st,
+                 int *capacity_ver) {
+  int status = 0;
+  int counter_axis = 0;
   ptr++;
   if (*ptr == ' ') {
     while (*ptr != '\0') {
       if (!isdigit(*ptr) && *ptr != '-') {
         ptr++;
       } else {
-        v->x = strtod(ptr, &ptr);
-        if(st->minmaxX[0]>v->x){
-          st->minmaxX[0]= v->x;
-          //Может не совсем правильно работать
-        } else if(st->minmaxX[1]<v->x){
-          st->minmaxX[1]= v->x;
-        }
-         
-        while (*ptr == ' ') {
-          ptr++;
-        }
-        v->y = strtod(ptr, &ptr);
-        if(st->minmaxY[0]>v->x){
-          st->minmaxY[0]= v->x;
-        } else if(st->minmaxY[1]<v->x){
-          st->minmaxY[1]= v->x;
-        }
-        while (*ptr == ' ') {
-          ptr++;
-        }
-        v->z = strtod(ptr, &ptr);
-        if(st->minmaxZ[0]>v->x){
-          st->minmaxZ[0]= v->x;
-        } else if(st->minmaxZ[1]<v->x){
-          st->minmaxZ[1]= v->x;
-        }
-      }
-    }
-  }
-}
-int memory_for_pol(char *ptr, poligons *p) {
-  int status = 0;
-  ptr++;
-  int amount_pol = 0;
-  while (*ptr != '\0') {
-    if (!isdigit(*ptr)) {
-      ptr++;
-    } else {
-      amount_pol++;
+        if (*vertex_counter < *capacity_ver) {
+          st->vertex[*vertex_counter] = strtod(ptr, &ptr);
+          if (*vertex_counter == 0) {
+            st->minmaxX[0] = st->vertex[*vertex_counter];
+            st->minmaxX[1] = st->vertex[*vertex_counter];
+          }
+          if (*vertex_counter == 1) {
+            st->minmaxY[0] = st->vertex[*vertex_counter];
+            st->minmaxY[1] = st->vertex[*vertex_counter];
+          }
+          if (*vertex_counter == 2) {
+            st->minmaxZ[0] = st->vertex[*vertex_counter];
+            st->minmaxZ[1] = st->vertex[*vertex_counter];
+          }
+          s21_minmax_cord(st, counter_axis, st->vertex[*vertex_counter]);
+          counter_axis++;
+          *vertex_counter += 1;
+          while (*ptr != '\0' && *ptr == ' ') {
+            ptr++;
+          }
+        } else {
+          double *tmp;
 
-      while (*ptr != ' ') {
-        ptr++;
+          *capacity_ver *= 2;
+
+          if ((tmp = realloc(st->vertex, *capacity_ver * sizeof(double))) ==
+
+              NULL) {
+            return 1;
+
+          } else {
+            st->vertex = tmp;
+          }
+        }
       }
     }
-  }
-  if ((p->poligon = (int *)calloc(amount_pol, sizeof(int))) == NULL) {
-    status = 1;
-  } else {
-    p->amount_pol = amount_pol;
   }
 
   return status;
 }
-void parse_poligons(char *ptr, poligons *p) {
+
+int parse_poligons(char *ptr, exit_st *st, int *poligons_counter,
+                   int *capacity_pol) {
   ptr++;
   int i = 0;
-  while (*ptr != '\0') {
-    if (!isdigit(*ptr)) {
-      ptr++;
-    } else {
-      p->poligon[i] = strtol(ptr, &ptr, 0);
-      i++;
-      while (*ptr != ' ') {
+  int status = 0;
+  int first_poligon = 0;
+  int tmp_pol = 0;
+  if (*ptr == ' ') {
+    while (*ptr != '\0') {
+      if (!isdigit(*ptr)) {
         ptr++;
+      } else {
+        if (*poligons_counter < *capacity_pol - 1) {
+          if (i == 0) {
+            st->poligons[*poligons_counter] = strtol(ptr, &ptr, 0) - 1;
+            first_poligon = st->poligons[*poligons_counter];
+            *poligons_counter += 1;
+            i++;
+          } else {
+            tmp_pol = strtol(ptr, &ptr, 0) - 1;
+            st->poligons[*poligons_counter] = tmp_pol;
+            *poligons_counter += 1;
+            st->poligons[*poligons_counter] = tmp_pol;
+            *poligons_counter += 1;
+          }
+          while (*ptr != '\0' && *ptr != ' ') {
+            ptr++;
+          }
+        } else {
+          unsigned *tmp;
+          *capacity_pol *= 2;
+          if ((tmp = realloc(st->poligons, *capacity_pol * sizeof(unsigned))) ==
+              NULL) {
+            return 1;
+          } else {
+            st->poligons = tmp;
+          }
+        }
       }
+    }
+    st->poligons[*poligons_counter] = first_poligon;
+    *poligons_counter += 1;
+  }
+  return status;
+}
+
+void s21_remove_struct(exit_st *st) {
+  free(st->vertex);
+  free(st->poligons);
+}
+
+void s21_minmax_cord(exit_st *st, int counter_axis, double coord) {
+  if (counter_axis == 0) {
+    if (st->minmaxX[0] > coord) {
+      st->minmaxX[0] = coord;
+    }
+    if (st->minmaxX[1] < coord) {
+      st->minmaxX[1] = coord;
+    }
+  } else if (counter_axis == 1) {
+    if (st->minmaxY[0] > coord) {
+      st->minmaxY[0] = coord;
+    }
+    if (st->minmaxY[1] < coord) {
+      st->minmaxY[1] = coord;
+    }
+  } else if (counter_axis == 2) {
+    if (st->minmaxZ[0] > coord) {
+      st->minmaxZ[0] = coord;
+    }
+    if (st->minmaxZ[1] < coord) {
+      st->minmaxZ[1] = coord;
     }
   }
 }
